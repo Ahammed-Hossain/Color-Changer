9447b024e3f3f88a97d29644714e77cc1f564676
Ahammed Hossain <ashikahmed3136@gmail.com>
2025-08-17 22:27:25 +0600

    Add color changer files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Color-Changer
 It's a simple color changing app using Html, Css and Javascript.
+<br>
+# Features
+- Randomly change the color
---
+++ script.js
@@ -0,0 +1,8 @@
+
+let button = document.querySelector("button");
+
+
+button.addEventListener("click", function() {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+});
